7af0cccfd6711a0593b43ebcd5a1c273139f50f4
Edvin Alicic <edvin.alicic2020@outlook.com>
2025-05-15 18:59:06 +0200

    Dto

---
+++ KursSistemDiplomskiRad/DTOs/KursDto.cs
@@ -0,0 +1,14 @@
+﻿namespace KursSistemDiplomskiRad.DTOs
+{
+    public class KursDto
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; } = string.Empty;
+        public string Opis { get; set; } = string.Empty;
+        public DateTime DatumPocetka { get; set; }
+        public DateTime DatumZavrsetka { get; set; }
+        public int BrojPolaznika { get; set; }
+        public List<LekcijeDto> Lekcije { get; set; } = new List<LekcijeDto>();
+        public List<StudentDto> Polaznici { get; set; } = new List<StudentDto>();
+    }
+}
---
+++ KursSistemDiplomskiRad/DTOs/StudentDto.cs
@@ -0,0 +1,19 @@
+﻿using KursSistemDiplomskiRad.Entities;
+
+namespace KursSistemDiplomskiRad.DTOs
+{
+    public class StudentDto
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Telefon { get; set; }
+        public string Adresa { get; set; }
+        public int BrojIndeksa { get; set; }
+        // Navigacione osobine
+        public ICollection<StudentKurs> StudentKursevi { get; set; } = new List<StudentKurs>();
+
+    }
+}
